ddec55fa3e899da6d61dda3e6cb1825bc4c29a84
toykam <toykampage1000@gmail.com>
2018-09-17 16:12:50 +0100

    Validation for empty input value

--- todo app/js/script.js
+++ todo app/js/script.js
@@ -1,14 +1,16 @@
 $(function() {
-    $("#add").on("click", function() {
-        var val = $("input").val();
-        if(val !== '') {
-            var elem = $("<li></li>").text(val);
-            $(elem).append("<button class='rem'>X</button>");
-            $("#mylist").append(elem);
-            $("input").val("");
-            $(".rem").on("click", function() {
-                $(this).parent().remove();
-            });
-        }
-    });
+	$("#add").on("click", function() {
+		var val = $("input").val();
+		if (val !== '') {
+			var elem = $("<li></li>").text(val);
+			$(elem).append("<button class='rem'>X</button>");
+			$("#mylist").append(elem);
+			$("input").val("");
+			$(".rem").on("click", function() {
+				$(this).parent().remove();
+			});
+		}else{
+			alert("To-do Name Must not be empty");
+		}
+	});
 });
