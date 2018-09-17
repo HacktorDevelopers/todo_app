d2da7393abfb37b987bf675bf3e8de47608a89a9
toykam <toykampage1000@gmail.com>
2018-09-17 16:02:35 +0100

    No design

--- todo app/js/script.js
+++ todo app/js/script.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-	alert("Hello You are welcome");
+$(function() {
+    $("#add").on("click", function() {
+        var val = $("input").val();
+        if(val !== '') {
+            var elem = $("<li></li>").text(val);
+            $(elem).append("<button class='rem'>X</button>");
+            $("#mylist").append(elem);
+            $("input").val("");
+            $(".rem").on("click", function() {
+                $(this).parent().remove();
+            });
+        }
+    });
 });
